c6b48cdb5ab1b95d73de5341415f6ddf8761add5
isato12 <isato122@gmail.com>
2022-03-15 10:36:34 -0600

    Terminando pagina landing

---
+++ js/app.js
@@ -0,0 +1,169 @@
+/** @format */
+
+const menu = document.querySelector(".hamburguesa");
+const navegacion = document.querySelector(".navegacion");
+const imagenes = document.querySelectorAll("img");
+const btnTodos = document.querySelector(".todos");
+const btnEnsaladas = document.querySelector(".ensaladas");
+const btnPasta = document.querySelector(".pasta");
+const btnPizza = document.querySelector(".pizza");
+const btnPostres = document.querySelector(".postres");
+const contenedorPlatillos = document.querySelector(".platillos");
+document.addEventListener("DOMContentLoaded", () => {
+  eventos();
+  platillos();
+  scrollNav();
+  navegacionFija();
+});
+
+const eventos = () => {
+  menu.addEventListener("click", abrirMenu);
+};
+
+const abrirMenu = () => {
+  navegacion.classList.remove("ocultar");
+  botonCerrar();
+};
+
+const botonCerrar = () => {
+  const btnCerrar = document.createElement("p");
+  const overlay = document.createElement("div");
+  overlay.classList.add("pantalla-completa");
+  const body = document.querySelector("body");
+  if (document.querySelectorAll(".pantalla-completa").length > 0) return;
+  body.appendChild(overlay);
+  btnCerrar.textContent = "x";
+  btnCerrar.classList.add("btn-cerrar");
+
+  // while(navegacion.children[5]){
+  //     navegacion.removeChild(navegacion.children[5]);
+  // }
+  navegacion.appendChild(btnCerrar);
+  cerrarMenu(btnCerrar, overlay);
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const imagen = entry.target;
+      imagen.src = imagen.dataset.src;
+      observer.unobserve(imagen);
+    }
+  });
+});
+
+imagenes.forEach((imagen) => {
+  observer.observe(imagen);
+});
+
+const cerrarMenu = (boton, overlay) => {
+  boton.addEventListener("click", () => {
+    navegacion.classList.add("ocultar");
+    overlay.remove();
+    boton.remove();
+  });
+
+  overlay.onclick = function () {
+    overlay.remove();
+    navegacion.classList.add("ocultar");
+    boton.remove();
+  };
+};
+
+const platillos = () => {
+  let platillosArreglo = [];
+  const platillos = document.querySelectorAll(".platillo");
+
+  platillos.forEach(
+    (platillo) => (platillosArreglo = [...platillosArreglo, platillo]),
+  );
+
+  const ensaladas = platillosArreglo.filter(
+    (ensalada) => ensalada.getAttribute("data-platillo") === "ensalada",
+  );
+  const pastas = platillosArreglo.filter(
+    (pasta) => pasta.getAttribute("data-platillo") === "pasta",
+  );
+  const pizzas = platillosArreglo.filter(
+    (pizza) => pizza.getAttribute("data-platillo") === "pizza",
+  );
+  const postres = platillosArreglo.filter(
+    (postre) => postre.getAttribute("data-platillo") === "postre",
+  );
+
+  mostrarPlatillos(ensaladas, pastas, pizzas, postres, platillosArreglo);
+};
+
+const mostrarPlatillos = (ensaladas, pastas, pizzas, postres, todos) => {
+  btnEnsaladas.addEventListener("click", () => {
+    limpiarHtml(contenedorPlatillos);
+    ensaladas.forEach((ensalada) => contenedorPlatillos.appendChild(ensalada));
+  });
+
+  btnPasta.addEventListener("click", () => {
+    limpiarHtml(contenedorPlatillos);
+    pastas.forEach((pasta) => contenedorPlatillos.appendChild(pasta));
+  });
+
+  btnPizza.addEventListener("click", () => {
+    limpiarHtml(contenedorPlatillos);
+    pizzas.forEach((pizza) => contenedorPlatillos.appendChild(pizza));
+  });
+  btnPostres.addEventListener("click", () => {
+    limpiarHtml(contenedorPlatillos);
+    postres.forEach((postre) => contenedorPlatillos.appendChild(postre));
+  });
+  btnTodos.addEventListener("click", () => {
+    limpiarHtml(contenedorPlatillos);
+    todos.forEach((todo) => contenedorPlatillos.appendChild(todo));
+  });
+};
+
+//
+//
+
+function navegacionFija() {
+  const barra = document.querySelector(".contenedor-navegacion");
+  const nosotros = document.querySelector(".contenedor-nosotros");
+  window.addEventListener("scroll", function () {
+    if (nosotros.getBoundingClientRect().top < 0) {
+      barra.classList.add("fijo");
+      console.log("fijo");
+    } else {
+      barra.classList.remove("fijo");
+      console.log("nofijo");
+    }
+  });
+  //   // Registrar el Intersection Observer
+  //   const observer = new IntersectionObserver(function (entries) {
+  //     if (entries[0].isIntersecting) {
+  //
+  //     } else {
+  //       barra.classList.add("fijo");
+  //     }
+  //   });
+  //   console.log(observer);
+
+  //   // Elemento a observar
+  //   observer.observe(document.querySelector(".video"));
+}
+
+function scrollNav() {
+  const enlaces = document.querySelectorAll(".navegacion a");
+
+  enlaces.forEach(function (enlace) {
+    enlace.addEventListener("click", function (e) {
+      e.preventDefault();
+      const seccion = document.querySelector(e.target.attributes.href.value);
+      seccion.scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+}
+
+const limpiarHtml = (contenedor) => {
+  while (contenedor.firstChild) {
+    contenedor.removeChild(contenedor.firstChild);
+  }
+};
